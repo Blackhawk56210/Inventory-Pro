28d126ce0c1a03e18e2468b24eaf9aa79280becd
Colby <maquiret@gmail.com>
2025-11-02 13:25:35 -0600

    ui changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '@/assets/pages/HomePage'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Banner from './Banner'
+import Table from './Table'
+
+function HomePage() {
+  return (
+    <>
+        <div>
+            <Banner />
+            <Table />
+        </div>
+    </>
+  )
+}
+
+export default HomePage
